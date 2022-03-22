9583e0235a1e457e44cf7f6c593c0c409e52523f
Yara Tavares <yaracriistavares@gmail.com>
2022-03-22 19:47:35 -0300

    feat: get the likes of the post and the user

--- src/controllers/likeController.js
+++ src/controllers/likeController.js
@@ -1,9 +1,33 @@
 import { connection } from "../database.js";
 
 export async function getLikes(req, res) {
+  // const {user} = res.locals; user virá dessa forma depois da rota login e exclui debaixo
+  const userId = 2;
+
   try {
-    const select = connection.query(`SELECT * FROM likes`);
-    res.status(200).send(select);
+    const requestLikes = await connection.query(
+      `SELECT COUNT("userId") AS "countLikes", "postId"  
+        FROM likes GROUP BY "postId"`
+    );
+
+    const likes = requestLikes.rows;
+
+    const postsIsLiked = await connection.query(
+      `SELECT * FROM likes WHERE "userId" = $1`,
+      [userId]
+    );
+
+    const userLikes = likes.map((post) => {
+      let liked = "";
+      if (post.postId.includes(postsIsLiked)) {
+        liked = true;
+      } else {
+        liked = false;
+      }
+      return { post, liked };
+    });
+
+    res.status(200).send(userLikes);
   } catch (err) {
     console.log(err);
     res.sendStatus(500);
@@ -26,10 +50,8 @@ export async function likePostOrNot(req, res) {
     }
 
     const postIsLike = await connection.query(
-      `SELECT posts.id, likes."userId" FROM posts 
-        JOIN likes ON likes."postId" = posts.id
-        WHERE posts.id = $1`,
-      [idPost]
+      `SELECT * FROM likes WHERE "postId" = $1 AND "userId" = $2`,
+      [idPost, userId]
     );
 
     if (postIsLike.rowCount > 0) {
--- src/routes/likeRouter.js
+++ src/routes/likeRouter.js
@@ -5,7 +5,7 @@ import likeSchema from "../schemas/likeSchema.js";
 
 const likeRouter = Router();
 
-likeRouter.get("/like/:idPost", getLikes);
+likeRouter.get("/like", getLikes);
 likeRouter.post(
   "/like/:idPost",
   validateSchemaMiddleware(likeSchema),
