d67497c5b8b8982dc09ae3372a8b01453671b283
Chaves.Rodrigo <rodrigoccavalcante@gmail.com>
2022-03-24 11:01:13 -0300

    add getPosts route

--- src/repositories/postRepository.js
+++ src/repositories/postRepository.js
@@ -5,6 +5,8 @@ async function getPosts(){
         SELECT po.*, pu.username, pu."image_url" 
         FROM posts po
         JOIN public_contents pu ON po."userId"=pu."userId"
+        ORDER BY po.post_date DESC
+        LIMIT 20
     `);
 }
 
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,19 +1,10 @@
 import { Router } from "express";
-<<<<<<< HEAD
 import postRouter from "./postRouter.js";
-
+import likeRouter from "./likeRouter.js";
 
 
 const router = Router();
 router.use(postRouter);
-
-
-export default router;
-=======
-import likeRouter from "./likeRouter.js";
-
-const router = Router();
 router.use(likeRouter);
 
 export default router;
->>>>>>> 7eeca73fa6f6bc57d94be48fcaf48b5c59609bd6
