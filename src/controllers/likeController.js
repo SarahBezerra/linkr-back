e486cd6ea998e7eb2139b9f6dcf03080a224e357
Yara Tavares <yaracriistavares@gmail.com>
2022-03-24 13:51:02 -0300

    fix: modifications in likeController

--- src/controllers/likeController.js
+++ src/controllers/likeController.js
@@ -2,33 +2,71 @@ import { connection } from "../database.js";
 
 export async function getLikes(req, res) {
   // const {user} = res.locals; user virá dessa forma depois da rota login e exclui debaixo
-  const userId = 1;
+  const theUser = 1;
 
   try {
     const requestLikes = await connection.query(
       `SELECT COUNT("userId") AS "countLikes", "postId"  
-        FROM likes GROUP BY "postId"`
+        FROM likes GROUP BY "postId"
+      ORDER BY "postId"`
     );
 
     const likes = requestLikes.rows;
 
     const requestPostsIsLiked = await connection.query(
       `SELECT "postId" as id FROM likes WHERE "userId" = $1`,
-      [userId]
+      [theUser]
     );
 
     const postIsLiked = requestPostsIsLiked.rows;
 
+    const requestUsersNames = await connection.query(
+      `SELECT likes."userId", likes."postId" as id,public_contents.username 
+        FROM public_contents 
+      JOIN likes ON likes."userId" = public_contents."userId"
+      ORDER BY likes."postId"
+      `
+    );
+
+    const userNames = requestUsersNames.rows;
+
     const userLikes = likes.map(({ countLikes, postId }) => {
       let liked = false;
-      postIsLiked.map((object) => {
-        object.id === postId ? (liked = true) : "";
+      postIsLiked.map(({ id }) => {
+        id === postId ? (liked = true) : "";
       });
 
-      return { countLikes, postId, liked };
+      const arrayUsersNames = [];
+
+      userNames.map(({ id, userId, username }) => {
+        if (id === postId && userId === theUser)
+          return arrayUsersNames.unshift("Você");
+        if (id === postId && arrayUsersNames.length < 3)
+          return arrayUsersNames.push(username);
+      });
+
+      const othersPeoples = countLikes - arrayUsersNames.length;
+      othersPeoples !== 0
+        ? arrayUsersNames.push(`and others ${othersPeoples} peoples`)
+        : "";
+
+      return { countLikes, postId, liked, arrayUsersNames };
     });
 
     res.status(200).send(userLikes);
+  } catch (err) {
+    console.log(err);
+    res.sendStatus(500);
+  }
+}
+
+export async function requesitionTest(req, res) {
+  try {
+    const test = request.rows[0];
+
+    const array = test.map(({ postId, username }) => {
+      return { postId, username };
+    });
   } catch (err) {
     console.log(err);
     res.sendStatus(500);
