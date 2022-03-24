4bfea4edc1ff01bd39c41dda551a08f222ad3ea2
Sarah <sarahbezerra2013@gmail.com>
2022-03-24 16:04:13 -0300

    route sign-up: validate schema

--- src/routes/authRoute.js
+++ src/routes/authRoute.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { postSingUp } from "../controllers/authController.js";
+import validateSchemaMiddleware from "../middlewares/validateSchemaMiddleware.js";
+import signUpSchema from "../schemas/signUpSchema.js";
 
 const authRouter = Router();
 
-authRouter.post('/sign-up', postSingUp);
+authRouter.post('/sign-up', validateSchemaMiddleware(signUpSchema), postSingUp);
 
 export default authRouter;
