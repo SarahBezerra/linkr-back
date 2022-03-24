e62606ce01230e4259f6d4b7168d4933bd681680
Yara Tavares <yaracriistavares@gmail.com>
2022-03-24 17:54:13 -0300

    docs: likeRepository doc

--- src/controllers/likeController.js
+++ src/controllers/likeController.js
@@ -1,32 +1,20 @@
 import { connection } from "../database.js";
+import { likeRepository } from "../repositories/likeRepository.js";
 
 export async function getLikes(req, res) {
   // const {user} = res.locals; user virá dessa forma depois da rota login e exclui debaixo
   const theUser = 1;
 
   try {
-    const requestLikes = await connection.query(
-      `SELECT COUNT("userId") AS "countLikes", "postId"  
-        FROM likes GROUP BY "postId"
-      ORDER BY "postId"`
-    );
+    const requestLikes = await likeRepository.countLikesPosts();
 
     const likes = requestLikes.rows;
 
-    const requestPostsIsLiked = await connection.query(
-      `SELECT "postId" as id FROM likes WHERE "userId" = $1`,
-      [theUser]
-    );
+    const requestPostsIsLiked = await likeRepository.theUserLiked(theUser);
 
     const postIsLiked = requestPostsIsLiked.rows;
 
-    const requestUsersNames = await connection.query(
-      `SELECT likes."userId", likes."postId" as id,public_contents.username 
-        FROM public_contents 
-      JOIN likes ON likes."userId" = public_contents."userId"
-      ORDER BY likes."postId"
-      `
-    );
+    const requestUsersNames = await likeRepository.namesLikesPosts();
 
     const userNames = requestUsersNames.rows;
 
@@ -60,51 +48,26 @@ export async function getLikes(req, res) {
   }
 }
 
-export async function requesitionTest(req, res) {
-  try {
-    const test = request.rows[0];
-
-    const array = test.map(({ postId, username }) => {
-      return { postId, username };
-    });
-  } catch (err) {
-    console.log(err);
-    res.sendStatus(500);
-  }
-}
-
 export async function likePostOrNot(req, res) {
   // const {user} = res.locals; user virá dessa forma depois da rota login e exclui debaixo
   const { userId } = req.body;
   const { idPost } = req.params;
 
   try {
-    const postExist = await connection.query(
-      `SELECT * FROM posts WHERE id = $1`,
-      [idPost]
-    );
+    const postExist = await likeRepository.verifyPostExist(idPost);
 
     if (!(postExist.rowCount > 0)) {
       return res.sendStatus(404);
     }
 
-    const postIsLike = await connection.query(
-      `SELECT * FROM likes WHERE "postId" = $1 AND "userId" = $2`,
-      [idPost, userId]
-    );
+    const postIsLike = await likeRepository.verifyPostLiked(idPost, userId);
 
     if (postIsLike.rowCount > 0) {
-      await connection.query(
-        `DELETE FROM likes WHERE "postId" = $1 AND "userId" = $2`,
-        [idPost, userId]
-      );
+      await likeRepository.deleteLike(idPost, userId);
       return res.sendStatus(200);
     }
 
-    await connection.query(
-      `INSERT INTO likes ("postId", "userId") VALUES ($1, $2)`,
-      [idPost, userId]
-    );
+    await likeRepository.insertLike(idPost, userId);
     res.sendStatus(201);
   } catch (err) {
     console.log(err);
