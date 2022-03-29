7e5c2ac906c88d54c0f13ab0dce342717a0aadef
Yara Tavares <yaracriistavares@gmail.com>
2022-03-29 18:14:32 -0300

    feat: router post in comments, new comment in post

--- src/controllers/commentController.js
+++ src/controllers/commentController.js
@@ -1,3 +1,32 @@
+import { connection } from "../database.js";
+
 export async function getInitComments(req, res) {
   res.sendStatus(200);
+}
+
+export async function postNewComment(req, res) {
+  const { user } = res.locals;
+  const { postId } = req.params;
+  const { text } = req.body;
+  const userId = user.id;
+  console.log(user.id);
+
+  try {
+    const postExist = await connection.query(
+      `SELECT * FROM posts WHERE id = $1`,
+      [postId]
+    );
+
+    if (!(postExist.rowCount > 0)) return res.sendStatus(404);
+
+    await connection.query(
+      `INSERT INTO comments ("userId", "postId", text) VALUES ($1, $2, $3)`,
+      [userId, postId, text]
+    );
+
+    res.sendStatus(201);
+  } catch (err) {
+    console.log(err);
+    res.sendStatus(500);
+  }
 }
--- src/routes/commentRouter.js
+++ src/routes/commentRouter.js
@@ -1,9 +1,20 @@
 import { Router } from "express";
-import { getInitComments } from "../controllers/commentController.js";
+import {
+  getInitComments,
+  postNewComment,
+} from "../controllers/commentController.js";
+import validateSchemaMiddleware from "../middlewares/validateSchemaMiddleware.js";
 import { validateToken } from "../middlewares/validateToken.js";
+import commentSchema from "../schemas/commentSchema.js";
 
 const commentRouter = Router();
 
 commentRouter.get("/comments", validateToken, getInitComments);
+commentRouter.post(
+  "/comments/:postId",
+  validateToken,
+  validateSchemaMiddleware(commentSchema),
+  postNewComment
+);
 
 export default commentRouter;
